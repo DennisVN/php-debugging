ffd2e050543e56f49f84245fbc7de5aad6b34e5d
DennisVN <d.vannieuwenhuyse@gmail.com>
2021-07-27 14:46:29 +0200

    Finished ex 04

--- resources/expert.php
+++ resources/expert.php
@@ -18,8 +18,14 @@ $monday = $week[0];
 echo $monday;
 
 new_exercise(3);
-// === Exercise 3 ===
-// This should echo ` "Debugged !" `, fix it so that that is the literal text echo'ed
 
 $str = "Debugged ! Also very fun";
 echo substr($str, 0, 10);
+
+new_exercise(4);
+
+foreach($week as &$day) {
+    $day = substr($day, 0, -3);
+}
+
+print_r($week);
