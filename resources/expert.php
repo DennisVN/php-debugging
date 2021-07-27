0d9a395676324c3919733a388e4550ce5b038b23
DennisVN <d.vannieuwenhuyse@gmail.com>
2021-07-27 11:16:47 +0200

    Debug ex 1 & 2 after completing first one

--- resources/expert.php
+++ resources/expert.php
@@ -1,7 +1,22 @@
 <?php
 declare(strict_types=1);
-function new_exercise() {
-    $block = "<br/><hr/><br/><br/>Exercise $x starts here:<br/>";
+
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+function new_exercise($x) {
+    $block = "<br/><hr/><br/><br/>Exercise {$x} starts here:<br/>";
     echo $block;
 }
-new_exercise();
+new_exercise(1);
+
+new_exercise(2);
+// === Exercise 2 ===
+// Below we create a week array with all days of the week.
+// We then try to print the first day which is monday, execute the code and see what happens.
+
+$week = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
+$monday = $week[1];
+
+echo $monday;
